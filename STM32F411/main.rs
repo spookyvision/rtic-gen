607d1c133832fdcbf5493f999a36c423bc7aa7fd
Anatol Ulrich <anatol.ulrich@ferrous-systems.com>
2022-02-01 02:13:24 +0100

    checkpoint

--- STM32F411/main.rs
+++ STM32F411/main.rs
@@ -2,13 +2,13 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_semihosting::{debug, hprintln};
-use panic_halt as _;
-use stm32f4::{stm32f411, stm32f411::Interrupt};
-
 // NOTE: does NOT properly work on QEMU
-#[rtic::app(device = stm32f411, monotonic = rtic::cyccnt::CYCCNT)]
-const APP: () = {
+#[rtic::app(device = stm32f411)]
+mod app {
+    use stm32f4::{stm32f411, stm32f411::Interrupt};
+    use stm32f4xx_hal as hal;
+
+    #[resources]
     struct Resources {
         // A resource
         #[init(0)]
@@ -16,23 +16,52 @@ const APP: () = {
     }
 
     #[init]
-    fn init(_: init::Context) {
+    fn init(_: init::Context) -> (init::LateResources, init::Monotonics) {
         // omitted: initialization of `CYCCNT`
         rtic::pend(Interrupt::USART1);
+        (init::LateResources {}, init::Monotonics())
     }
 
     #[idle]
     fn idle(_cx: idle::Context) -> ! {
-        //debug::exit(debug::EXIT_SUCCESS);
+        //foo::schedule(/* ... */).unwrap();
 
         loop {}
+        // exit();
     }
 
     #[task(binds = USART1, resources = [shared])]
-    fn usart1(cx: usart1::Context) {
-        let shared: &mut u32 = cx.resources.shared;
-        *shared += 1;
-
-        hprintln!("UART0: shared = {}", shared).unwrap();
+    fn usart1(mut cx: usart1::Context) {
+        cx.resources.shared.lock(|shared| {
+            *shared += 1;
+            defmt::debug!("USART1: shared = {}", shared);
+        });
     }
-};
+
+    use core::sync::atomic::{AtomicUsize, Ordering};
+
+    use defmt_rtt as _; // global logger
+    use panic_probe as _;
+
+    // same panicking *behavior* as `panic-probe` but doesn't print a panic message
+    // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+    #[defmt::panic_handler]
+    fn panic() -> ! {
+        cortex_m::asm::udf()
+    }
+
+    static COUNT: AtomicUsize = AtomicUsize::new(0);
+    defmt::timestamp!("{=usize}", {
+        // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+        let n = COUNT.load(Ordering::Relaxed);
+        COUNT.store(n + 1, Ordering::Relaxed);
+        n
+    });
+
+    /// Terminates the application and makes `probe-run` exit with exit-code = 0
+    pub fn exit() -> ! {
+        loop {
+            cortex_m::asm::bkpt();
+        }
+    }
+}
